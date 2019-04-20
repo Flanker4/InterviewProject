94b01055a7d055ea3e94c48a6ec62b3cfff02303
Andrii Boiko <boykoav4@gmail.com>
2019-04-20 21:03:36 +0300

    Initial Commit

---
+++ InterviewProject/MasterViewController.h
@@ -0,0 +1,24 @@
+//
+//  MasterViewController.h
+//  InterviewProject
+//
+//  Created by Andrey Boyko on 4/20/19.
+//  Copyright © 2019 Andrey Boyko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+#import "InterviewProject+CoreDataModel.h"
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController<Event *> *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
